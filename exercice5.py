d21a7d261dade3d4d90378ab372ffa5b286e5f34
houcine-sys <loopez415@gmail.com>
2026-01-28 14:36:16 +0100

    test commit
    commit

---
+++ exercice5.py
@@ -0,0 +1,9 @@
+
+def trouver_maximum(liste):
+    if not liste:
+        return None
+    return max(liste)
+
+# Bach t-tésti (ikhtiyari):
+nombres = [10, 5, 20, 8]
+print(trouver_maximum(nombres))
